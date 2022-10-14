7db0e8bffc96610967bd29e76d85132f14b5a925
SpiReCZ <SpiReCZ@users.noreply.github.com>
2022-10-14 20:48:11 +0200

    minimize changes

--- tor.native/src/main/kotlin/org/berndpruenster/netlayer/tor/TorContext.kt
+++ tor.native/src/main/kotlin/org/berndpruenster/netlayer/tor/TorContext.kt
@@ -96,8 +96,6 @@ class Torrc @Throws(IOException::class) internal constructor(defaults: InputStre
             return ByteArrayInputStream(outputStream.toByteArray())
         }
 
-    internal val runAsDaemon = rc.getOrDefault("RunAsDaemon", "0").toBoolean()
-
     companion object {
         @Throws(IOException::class)
         private fun parse(src: InputStream?): LinkedHashMap<String, String>? {
@@ -177,7 +175,6 @@ abstract class TorContext @Throws(IOException::class) protected constructor(val 
     internal val torrcFile = File(workingDirectory, FILE_TORRC)
     internal val torExecutableFile get() = File(workingDirectory, torExecutableFileName)
     internal val cookieFile = File(workingDirectory, FILE_AUTH_COOKIE)
-    private var runAsDaemon: Boolean = false
 
     @Throws(IOException::class)
     open fun installFiles() {
@@ -229,11 +226,8 @@ abstract class TorContext @Throws(IOException::class) protected constructor(val 
         getByName(FILE_TORRC).use { str ->
             Torrc(str, overrides).inputStream.use { rc ->
                 getByName(FILE_TORRC_DEFAULTS).use {
-                    runAsDaemon = with(Torrc(rc, it)) {
-                        this.inputStream.use {
-                            cleanInstallFile(it, torrcFile)
-                        }
-                        this.runAsDaemon
+                    Torrc(rc, it).inputStream.use {
+                        cleanInstallFile(it, torrcFile)
                     }
                 }
             }
@@ -360,7 +354,7 @@ abstract class TorContext @Throws(IOException::class) protected constructor(val 
             // This does create a condition where the process has exited due to
             // a problem but we should hopefully
             // detect that when we try to use the control connection.
-            if (OsType.current != OsType.WIN && runAsDaemon) {
+            if (OsType.current != OsType.WIN) {
                 val exit = torProcess.waitFor()
                 torProcess = null
                 if (exit != 0) {
